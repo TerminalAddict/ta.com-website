3fd19fcdabd6504b0efb9ef2f34d1b53c4f8d8ad
Paul Willard <paul@paulwillard.nz>
2018-11-05 22:21:04 +1300

    change caching

--- service_worker.js
+++ service_worker.js
@@ -11,7 +11,7 @@ var urlsToCache = [];
 urlsToCache.push('/assets/js/bundle.js');
 
 
-var CACHE_NAME = '{{ site.site_name | slugify }}-cache-v1';
+var CACHE_NAME = '{{ site.site_name | slugify }}-cache-{{ "now" | date: "%d-%m-%Y_%H:%M" }}';
 
 self.addEventListener('install', function(event) {
   // Perform install steps
